7d9b8664df15a89fda10236cf07c3dca889e6273
felixM-ha <din epost>
2025-10-22 11:04:42 +0300

    Negative RedFace

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -6,7 +6,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class CalculatorTest {
 
     //TODO
-    // Should add two positive numbers
     // Should add zero to a number
     // Should add negative numbers
 
@@ -15,5 +14,19 @@ public class CalculatorTest {
         Calculator calc = new Calculator();
         int result = calc.add(2, 3);
         assertEquals(5, result);
+    }
+
+    @Test
+    void AddZero() {
+        Calculator calc = new Calculator();
+        int result = calc.add(0, 2);
+        assertEquals(2, result);
+    }
+
+    @Test
+    void AddNegativeNumbers() {
+        Calculator calc = new Calculator();
+        int result = calc.add(-2, -3);
+        assertEquals(2, result);
     }
 }
